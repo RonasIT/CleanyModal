bd250f3679cccbc3dcfc557c200b2b5e7c0b2293
Dmitry Frishbuter <dfrishbuter@ronasit.com>
2020-07-23 17:38:51 +0600

    Hide default separators

--- CleanyModal/Classes/CleanyAlertViewController.swift
+++ CleanyModal/Classes/CleanyAlertViewController.swift
@@ -135,6 +135,8 @@ open class CleanyAlertViewController: CleanyModalViewController {
             iconIV.removeFromSuperview()
         }
         
+        actionsTV.separatorColor = .clear
+
         handleTableViewActions()
         applyStyle()
         
