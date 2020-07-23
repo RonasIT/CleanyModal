969e608467fe84b4bc50fecb33c9038cea669bc8
Dmitry Frishbuter <dfrishbuter@ronasit.com>
2020-07-23 15:31:15 +0600

    Refactor content view

--- CleanyModal/Classes/CleanyAlertViewController.swift
+++ CleanyModal/Classes/CleanyAlertViewController.swift
@@ -11,6 +11,27 @@ private let kFooterMargin: CGFloat = 0
 private let kCellReuseIdentifier = "actionCell"
 private let kCellDefaultHeight: CGFloat = 44.0
 
+final class CleanyContentView: UIView {
+
+    var separatorColor: UIColor?
+    var separatorPosition: CGPoint = .zero
+
+    override func draw(_ rect: CGRect) {
+        guard let color = separatorColor, let context = UIGraphicsGetCurrentContext() else {
+            return
+        }
+        context.saveGState()
+        context.setLineWidth(0.5)
+        context.setStrokeColor(color.cgColor)
+
+        context.move(to: CGPoint(x: separatorPosition.x, y: separatorPosition.y))
+        context.addLine(to: CGPoint(x: bounds.size.width, y: separatorPosition.y))
+
+        context.strokePath()
+        context.restoreGState()
+    }
+}
+
 open class CleanyAlertViewController: CleanyModalViewController {
     
     public enum Style: IntegerLiteralType {
